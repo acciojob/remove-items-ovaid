24789963281fc63da2342c9f41374e4526d171c8
RAMEEZ RAJA <84043654+ovaid@users.noreply.github.com>
2023-05-23 15:54:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+ function deleteEle() {
+  var selectElement = document.getElementById("colorSelect");
+  var selectChildren = selectElement.children[0];
+	 selectChildren.remove();
+}
